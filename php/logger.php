710c32a0fe7e5a128b42f55f7baca7daf198b7d3
BenOverholts <commandershadow@gmail.com>
2014-11-20 11:55:12 -0500

    PHP warning fixes

--- php/logger.php
+++ php/logger.php
@@ -3,13 +3,12 @@
     $ipaddress = $_SERVER['REMOTE_ADDR'];
     $page = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
     $page .= (!empty($_SERVER['QUERY_STRING']) ? "?{$_SERVER['QUERY_STRING']}" : "");
-    $referrer = $_SERVER['HTTP_REFERER'];
-    $datetime = mktime();
+    $datetime = time();
     $useragent = $_SERVER['HTTP_USER_AGENT'];
     $remotehost = @getHostByAddr($ipaddress);
 
     // Create log line
-    $logline = $ipaddress . " | " . $referrer . " | " . $datetime . " | " . $useragent . " | " . $remotehost . " | " . $page . "\n";
+    $logline = $ipaddress . " | " . $datetime . " | " . $useragent . " | " . $remotehost . " | " . $page . "\n";
 
     // Write to log file:
     $logfile = './log/logfile.txt';
