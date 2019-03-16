4d9c0cd4beea0e64b7863306c978a2e937d5cd41
Ben Overholts <over@amazon.com>
2019-03-16 10:49:14 -0700

    Remove logging from cycle children script

--- assets/js/cycle-children.js
+++ assets/js/cycle-children.js
@@ -1,12 +1,10 @@
 // TODO - support arbitrary number of usages on single page
 $(document).ready(function() {
     var elements = $('.cycle-children').children();
-    console.log(elements);
     var i = 0;
 
     function cycle() {
-        console.log("Cycling!");
-        elements.eq(i).toggleClass("active"); 
+        elements.eq(i).toggleClass("active");
         i = ++i % elements.length;
         elements.eq(i).toggleClass("active");
     };
